f20bd9a738b88d13b2b0a433221a736bb3c5de56
Antonio Quartulli <antonio@open-mesh.com>
2013-05-20 20:35:52 +0200

    batctl: beautify bisect_iv code
    This is a first tentative of beautification of the code in
    the bisect_iv.c file
    
    There is no behavioural change.
    
    Signed-off-by: Antonio Quartulli <antonio@open-mesh.com>

--- bisect_iv.c
+++ bisect_iv.c
@@ -51,13 +51,14 @@ static int bisect_iv_parse_log_file(char *file_path)
 	FILE *fd;
 	char line_buff[MAX_LINE], *start_ptr, *start_ptr_safe, *tok_ptr;
 	char *neigh, *iface_addr, *orig, *prev_sender, rt_flag;
-	int line_count = 0, tq, ttl, i, res, max;
+	int line_count = 0, tq, ttl, i, res, max, len;
 	long long seqno;
 
 	fd = fopen(file_path, "r");
 
 	if (!fd) {
-		fprintf(stderr, "Error - could not open file '%s': %s\n", file_path, strerror(errno));
+		fprintf(stderr, "Error - could not open file '%s': %s\n",
+			file_path, strerror(errno));
 		return 0;
 	}
 
@@ -91,7 +92,8 @@ static int bisect_iv_parse_log_file(char *file_path)
 					break;
 				case 14:
 					prev_sender = tok_ptr;
-					prev_sender[strlen(prev_sender) - 1] = 0;
+					len = strlen(prev_sender) - 1;
+					prev_sender[len] = 0;
 					break;
 				case 16:
 					seqno = strtoll(tok_ptr, NULL, 10);
@@ -106,7 +108,9 @@ static int bisect_iv_parse_log_file(char *file_path)
 			}
 
 			if (ttl ==  -1) {
-				fprintf(stderr, "Broken 'received packet' line found - skipping [file: %s, line: %i]\n", file_path, line_count);
+				fprintf(stderr,
+					"Broken 'received packet' line found - skipping [file: %s, line: %i]\n",
+					file_path, line_count);
 				continue;
 			}
 
@@ -116,7 +120,9 @@ static int bisect_iv_parse_log_file(char *file_path)
 						     prev_sender, neigh, seqno,
 						     tq, ttl);
 			if (res < 1)
-				fprintf(stderr, " [file: %s, line: %i]\n", file_path, line_count);
+				fprintf(stderr,
+					" [file: %s, line: %i]\n", file_path,
+					line_count);
 
 		} else if (strstr(start_ptr, "Adding route towards") ||
 			   strstr(start_ptr, "Changing route towards") ||
@@ -158,7 +164,8 @@ static int bisect_iv_parse_log_file(char *file_path)
 					break;
 				case 9:
 					prev_sender = tok_ptr;
-					prev_sender[strlen(prev_sender) - 2] = 0;
+					len = strlen(prev_sender) - 2;
+					prev_sender[len] = 0;
 					break;
 				}
 			}
@@ -169,7 +176,8 @@ static int bisect_iv_parse_log_file(char *file_path)
 			if (((rt_flag == RT_FLAG_ADD) && (!neigh)) ||
 			    ((rt_flag == RT_FLAG_UPDATE) && (!prev_sender)) ||
 			    ((rt_flag == RT_FLAG_DELETE) && (!orig))) {
-				fprintf(stderr, "Broken '%s route' line found - skipping [file: %s, line: %i]\n",
+				fprintf(stderr,
+					"Broken '%s route' line found - skipping [file: %s, line: %i]\n",
 				        (rt_flag == RT_FLAG_UPDATE ? "changing" :
 				        (rt_flag == RT_FLAG_ADD ? "adding" : "deleting")),
 				        file_path, line_count);
@@ -179,7 +187,8 @@ static int bisect_iv_parse_log_file(char *file_path)
 			res = bisect_routing_table_new(orig, neigh, prev_sender,
 						       rt_flag);
 			if (res < 1)
-				fprintf(stderr, " [file: %s, line: %i]\n", file_path, line_count);
+				fprintf(stderr, " [file: %s, line: %i]\n",
+					file_path, line_count);
 		}
 	}
 
@@ -190,11 +199,12 @@ static int bisect_iv_parse_log_file(char *file_path)
 
 int bisect_iv(int argc, char **argv)
 {
-	int ret = EXIT_FAILURE, res, optchar, found_args = 1;
-	int read_opt = USE_BAT_HOSTS, num_parsed_files;
-	long long tmp_seqno, seqno_max = -1, seqno_min = -1;
 	char *trace_orig_ptr = NULL, *rt_orig_ptr = NULL, *loop_orig_ptr = NULL;
-	char orig[NAME_LEN], filter_orig[NAME_LEN], *dash_ptr, *filter_orig_ptr = NULL;
+	char orig[NAME_LEN], filter_orig[NAME_LEN], *dash_ptr;
+	int ret = EXIT_FAILURE, res, optchar, found_args = 1;
+	long long tmp_seqno, seqno_max = -1, seqno_min = -1;
+	int read_opt = USE_BAT_HOSTS, num_parsed_files;
+	char *filter_orig_ptr = NULL;
 
 	memset(orig, 0, NAME_LEN);
 	memset(filter_orig, 0, NAME_LEN);
@@ -206,7 +216,7 @@ int bisect_iv(int argc, char **argv)
 			return EXIT_SUCCESS;
 		case 'l':
 			loop_orig_ptr = optarg;
-			found_args += ((*((char*)(optarg - 1)) == optchar ) ? 1 : 2);
+			found_args += ((*(optarg - 1) == optchar ) ? 1 : 2);
 			break;
 		case 'n':
 			read_opt &= ~USE_BAT_HOSTS;
@@ -214,11 +224,11 @@ int bisect_iv(int argc, char **argv)
 			break;
 		case 'o':
 			filter_orig_ptr = optarg;
-			found_args += ((*((char*)(optarg - 1)) == optchar ) ? 1 : 2);
+			found_args += ((*(optarg - 1) == optchar ) ? 1 : 2);
 			break;
 		case 'r':
 			rt_orig_ptr = optarg;
-			found_args += ((*((char*)(optarg - 1)) == optchar ) ? 1 : 2);
+			found_args += ((*(optarg - 1) == optchar ) ? 1 : 2);
 			break;
 		case 's':
 			dash_ptr = strchr(optarg, '-');
@@ -229,23 +239,28 @@ int bisect_iv(int argc, char **argv)
 			if ((tmp_seqno >= 0) && (tmp_seqno <= UINT32_MAX))
 				seqno_min = tmp_seqno;
 			else
-				fprintf(stderr, "Warning - given sequence number is out of range: %lli\n", tmp_seqno);
+				fprintf(stderr,
+					"Warning - given sequence number is out of range: %lli\n",
+					tmp_seqno);
 
 			if (dash_ptr) {
 				tmp_seqno = strtol(dash_ptr + 1, NULL , 10);
-				if ((tmp_seqno >= 0) && (tmp_seqno <= UINT32_MAX))
+				if ((tmp_seqno >= 0) &&
+				    (tmp_seqno <= UINT32_MAX))
 					seqno_max = tmp_seqno;
 				else
-					fprintf(stderr, "Warning - given sequence number is out of range: %lli\n", tmp_seqno);
+					fprintf(stderr,
+						"Warning - given sequence number is out of range: %lli\n",
+						tmp_seqno);
 
 				*dash_ptr = '-';
 			}
 
-			found_args += ((*((char*)(optarg - 1)) == optchar ) ? 1 : 2);
+			found_args += ((*(optarg - 1) == optchar ) ? 1 : 2);
 			break;
 		case 't':
 			trace_orig_ptr = optarg;
-			found_args += ((*((char*)(optarg - 1)) == optchar ) ? 1 : 2);
+			found_args += ((*(optarg - 1) == optchar ) ? 1 : 2);
 			break;
 		default:
 			bisect_iv_usage();
@@ -254,7 +269,8 @@ int bisect_iv(int argc, char **argv)
 	}
 
 	if (argc <= found_args + 1) {
-		fprintf(stderr, "Error - need at least 2 log files to compare\n");
+		fprintf(stderr,
+			"Error - need at least 2 log files to compare\n");
 		bisect_iv_usage();
 		goto err;
 	}
@@ -268,27 +284,27 @@ int bisect_iv(int argc, char **argv)
 	num_parsed_files = 0;
 
 	if ((rt_orig_ptr) && (trace_orig_ptr)) {
-		fprintf(stderr, "Error - the 'print routing table' option can't be used together with the 'trace seqno' option\n");
+		fprintf(stderr,
+			"Error - the 'print routing table' option can't be used together with the 'trace seqno' option\n");
 		goto err;
 	} else if ((loop_orig_ptr) && (trace_orig_ptr)) {
-		fprintf(stderr, "Error - the 'loop detection' option can't be used together with the 'trace seqno' option\n");
+		fprintf(stderr,
+			"Error - the 'loop detection' option can't be used together with the 'trace seqno' option\n");
 		goto err;
 	} else if ((loop_orig_ptr) && (rt_orig_ptr)) {
-		fprintf(stderr, "Error - the 'loop detection' option can't be used together with the 'print routing table' option\n");
+		fprintf(stderr,
+			"Error - the 'loop detection' option can't be used together with the 'print routing table' option\n");
 		goto err;
 	} else if (rt_orig_ptr) {
 		res = bisect_get_orig_addr(rt_orig_ptr, orig);
-
 		if (res < 1)
 			goto err;
 	} else if (trace_orig_ptr) {
 		res = bisect_get_orig_addr(trace_orig_ptr, orig);
-
 		if (res < 1)
 			goto err;
 	} else if (loop_orig_ptr) {
 		res = bisect_get_orig_addr(loop_orig_ptr, orig);
-
 		if (res < 1)
 			goto err;
 	}
@@ -298,7 +314,8 @@ int bisect_iv(int argc, char **argv)
 		seqno_max = seqno_min;
 
 	if (seqno_min > seqno_max) {
-		fprintf(stderr, "Error - the sequence range minimum (%lli) should be smaller than the maximum (%lli)\n",
+		fprintf(stderr,
+			"Error - the sequence range minimum (%lli) should be smaller than the maximum (%lli)\n",
 		       seqno_min, seqno_max);
 		goto err;
 	}
@@ -320,7 +337,8 @@ int bisect_iv(int argc, char **argv)
 	}
 
 	if (num_parsed_files < 2) {
-		fprintf(stderr, "Error - need at least 2 log files to compare\n");
+		fprintf(stderr,
+			"Error - need at least 2 log files to compare\n");
 		goto err;
 	}
 
